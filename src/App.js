5b13e42c25f50ac77b5049bdfb71afbb7ba08408
MacAposAGH <mapostol@student.agh.edu.pl>
2026-05-16 10:06:39 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import './App.css';
+import "milligram";
 import {useState} from "react";
+import LoginForm from "./LoginForm";
 
 function App() {
-    const [ email, setEmail] = useState("test.email@gmail.com")
+    const [logged, setLogged] = useState(false)
+    const [email, setEmail] = useState("test.email@gmail.com")
+    let text = email.length < 5 ? "Ale masz krótki adres!" : `Twój e-mail to ${email}`
 
     const handleChange = e => {
         setEmail(e.target.value)
     }
 
-    let text = email.length < 5 ? "Ale masz krótki adres!" : `Twój e-mail to ${email}`
+    const handleClick = () => {
+        if (!logged) {
+            window.alert(email)
+        }
+        setLogged(!logged)
+    }
+
+    const login = (t) => {
+        console.log(t)
+    }
 
     return (
-        <>
-            <div>System do zapisu zdjęć</div>
-            <h2>{text}</h2>
-            <input type="text" value={email} onChange={handleChange}/>
-        </>
+        <LoginForm onLogin={login} hidden={logged}/>
+
+        // <>
+        //     <div>System do zapisu zdjęć</div>
+        //     <h2>{text}</h2>
+        //     <input type="text" value={email} onChange={handleChange} hidden={logged}/>
+        //     <div hidden={!logged}>Witaj {email}!</div>
+        //     <button onClick={handleClick}>{logged ? "Wyloguj" : "Wchodzę"}</button>
+        // </>
     );
 }
 
